c53321e2c1db9c7d150508fbc063e41a3b9ea65e
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-07 18:28:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  
+]
+superbowlWin(record);
+
+function superbowlWin(arg){
+  let win=arg.find(function(item){
+    let winer=item.result==="W";
+    return winer
+  })
+console.log(win.year)
+if(typeof win ==="undefined"){return "undefined"}
+else{return win.year}
+}
+  // if() {return item.year }
+  // else{return console.log("undefined") }
+    
+ 
+
+
+
